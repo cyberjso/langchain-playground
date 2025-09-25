a36ba10ae642042bc69ebfff23172df95b2c1232
cyberjso <cyberjso@users.noreply.github.com~>
2025-09-24 22:13:04 -0300

    Update model version in factor_model.py and hello.py to gemini-2.5-flash

--- factor_model.py
+++ factor_model.py
@@ -3,9 +3,9 @@ from langchain.chat_models import init_chat_model
 from dotenv import load_dotenv
 
 load_dotenv()
-
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
 gemini_model = init_chat_model(model = "gemini-2.5-flash", model_provider="google_genai", temperature=0.7)
 
 answer = gemini_model.invoke("Hello, world!")
--- hello.py
+++ hello.py
@@ -2,14 +2,11 @@ import logging
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-
 load_dotenv()
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-
-model = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.7)
+model = ChatGoogleGenerativeAI(model = "gemini-2.5-flash", temperature = 0.7)
 message  = model.invoke("Hello, world!")
-
 
 logger.info(message.content)
