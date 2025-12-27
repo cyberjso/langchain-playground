a7b2c9263880ad831d9ce9b05fe5dc0920d862fc
cyberjso <cyberjso@users.noreply.github.com~>
2025-12-27 10:42:03 -0300

    Add initial implementation of JSON schema evaluation module with dataset handling and code review prompt

---
+++ evalutation/json-schema-eval/app.py
@@ -0,0 +1,80 @@
+import logging
+logging.basicConfig(level = logging.INFO)
+
+
+from os import getenv
+from dotenv import load_dotenv
+from pathlib import Path
+from langsmith.evaluation import LangChainStringEvaluator
+from langsmith.wrappers import wrap_openai
+from langchain.prompts import load_prompt
+from langsmith import evaluate
+from langsmith import Client as LangSmithClient
+
+from openai import OpenAI
+from pydantic import BaseModel, Field
+from typing import List, Dict
+from json import loads, JSONDecodeError
+from jsonschema import ValidationError, validate
+from dataset import prepare_dataset
+
+
+
+DATASET_NAME = "json-schema-code-review"
+MODEL_NAME = "gpt-4"
+TEMPERATURE = 0
+
+logger = logging.getLogger(__name__)
+
+load_dotenv(dotenv_path=Path(__file__).parent / ".env")
+
+class Findings(BaseModel):
+    description: str = Field(..., description="A summary of the code review findings.")
+    file_name: str = Field(..., description="The name of the file where the issue was found.")
+    issue_name: str = Field(..., description="The name of the identified issue.")
+
+class CodeReview(BaseModel):
+    findings: List[Findings] = Field(..., description="A summary of the code review findings.")    
+
+open_ai = wrap_openai(OpenAI())
+prompt =load_prompt(Path(__file__).parent / "prompt.yaml")
+
+LangChainStringEvaluator("json_validity", prepare_data = None)
+
+def __execute_prompt(input: Dict[str, str], input_key: str) -> Dict[str, str]:
+    text_message = prompt.format(**{input_key: input[input_key]})
+    
+    response = open_ai.chat.completions.create(
+        model = MODEL_NAME,
+        messages = [{"role": "user", "content": text_message}],
+        temperature = TEMPERATURE
+    )
+
+    return {"output": response.choices[0].message.content}
+
+def __format_evaluation_input(example: Dict) -> Dict:
+    return __execute_prompt(input =  example, input_key = "code")
+
+def __validate_schema(run, esx):
+    try:
+        data = loads(run.output.get("output"))
+        validate(instance = data, schema = CodeReview.model_json_schema())
+
+        return {"score": 1.0, "details": "Output matches the expected schema."}
+    except JSONDecodeError as e:
+        return {"score": 0.0, "details": f"Invalid Json Payload: {str(e)}"}
+    except ValidationError as e:
+        return {"score": 0.0, "details": f"JSON decode error: {str(e)}"}
+
+dataset_client = LangSmithClient()
+dataset_preparation_report  =  prepare_dataset(dataset_name = DATASET_NAME, 
+                                               client = dataset_client, 
+                                               dataset_file_path = Path(__file__).parent / getenv("DATASET_FILE_NAME", "json_schema_validation_dataset.jsonl"))
+
+logger.info(f"Dataset preparation report: {dataset_preparation_report}")
+
+# result = evaluate(__format_evaluation_input, 
+#                   data = DATASET_NAME, 
+#                   evaluators = [__validate_schema, LangChainStringEvaluator("json-validation", prepare_data = None)], 
+#                   experiment_prefix = "FormatEvaluation", 
+#                   max_concurrency = 1)
