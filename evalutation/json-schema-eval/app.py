eab1e6f1cce313f24e53e5e282d5fb3596b09459
cyberjso <cyberjso@users.noreply.github.com~>
2025-12-27 18:35:09 -0300

    Refactor logging setup and enhance prediction handling in JSON schema evaluation

--- evalutation/json-schema-eval/app.py
+++ evalutation/json-schema-eval/app.py
@@ -1,6 +1,4 @@
 import logging
-logging.basicConfig(level = logging.INFO)
-
 
 from os import getenv
 from dotenv import load_dotenv
@@ -18,15 +16,13 @@ from json import loads, JSONDecodeError
 from jsonschema import ValidationError, validate
 from dataset import prepare_dataset
 
-
+logging.basicConfig(level = logging.INFO)
+logger = logging.getLogger(__name__)
+load_dotenv(dotenv_path=Path(__file__).parent / ".env")
 
 DATASET_NAME = "json-schema-code-review"
 MODEL_NAME = "gpt-4"
 TEMPERATURE = 0
-
-logger = logging.getLogger(__name__)
-
-load_dotenv(dotenv_path=Path(__file__).parent / ".env")
 
 class Findings(BaseModel):
     description: str = Field(..., description="A summary of the code review findings.")
@@ -39,7 +35,8 @@ class CodeReview(BaseModel):
 open_ai = wrap_openai(OpenAI())
 prompt =load_prompt(Path(__file__).parent / "prompt.yaml")
 
-LangChainStringEvaluator("json_validity", prepare_data = None)
+def __prepare_prediction(run, example):
+    return {"prediction": run.outputs.get("output")}
 
 def __execute_prompt(input: Dict[str, str], input_key: str) -> Dict[str, str]:
     text_message = prompt.format(**{input_key: input[input_key]})
@@ -57,7 +54,7 @@ def __format_evaluation_input(example: Dict) -> Dict:
 
 def __validate_schema(run, esx):
     try:
-        data = loads(run.output.get("output"))
+        data = loads(run.outputs.get("output"))
         validate(instance = data, schema = CodeReview.model_json_schema())
 
         return {"score": 1.0, "details": "Output matches the expected schema."}
@@ -66,15 +63,13 @@ def __validate_schema(run, esx):
     except ValidationError as e:
         return {"score": 0.0, "details": f"JSON decode error: {str(e)}"}
 
-dataset_client = LangSmithClient()
 dataset_preparation_report  =  prepare_dataset(dataset_name = DATASET_NAME, 
-                                               client = dataset_client, 
+                                               client = LangSmithClient(), 
                                                dataset_file_path = Path(__file__).parent / getenv("DATASET_FILE_NAME", "json_schema_validation_dataset.jsonl"))
-
 logger.info(f"Dataset preparation report: {dataset_preparation_report}")
 
-# result = evaluate(__format_evaluation_input, 
-#                   data = DATASET_NAME, 
-#                   evaluators = [__validate_schema, LangChainStringEvaluator("json-validation", prepare_data = None)], 
-#                   experiment_prefix = "FormatEvaluation", 
-#                   max_concurrency = 1)
+result = evaluate(__format_evaluation_input, 
+                  data = DATASET_NAME, 
+                  evaluators = [__validate_schema, LangChainStringEvaluator("json_validity", prepare_data = __prepare_prediction)], 
+                  experiment_prefix = "FormatEvaluation", 
+                  max_concurrency = 1)
