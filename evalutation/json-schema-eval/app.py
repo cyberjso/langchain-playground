b599dab2577cc54837596ec1239df92e0daa13c7
cyberjso <cyberjso@users.noreply.github.com~>
2025-12-27 23:00:13 -0300

    Refactor configuration handling: replace hardcoded values with environment variables for dataset and model settings

--- evalutation/json-schema-eval/app.py
+++ evalutation/json-schema-eval/app.py
@@ -20,10 +20,6 @@ logging.basicConfig(level = logging.INFO)
 logger = logging.getLogger(__name__)
 load_dotenv(dotenv_path=Path(__file__).parent / ".env")
 
-DATASET_NAME = "json-schema-code-review"
-MODEL_NAME = "gpt-4"
-TEMPERATURE = 0
-
 class Findings(BaseModel):
     description: str = Field(..., description="A summary of the code review findings.")
     file_name: str = Field(..., description="The name of the file where the issue was found.")
@@ -33,7 +29,7 @@ class CodeReview(BaseModel):
     findings: List[Findings] = Field(..., description="A summary of the code review findings.")    
 
 open_ai = wrap_openai(OpenAI())
-prompt =load_prompt(Path(__file__).parent / "prompt.yaml")
+prompt = load_prompt(Path(__file__).parent / "prompt.yaml")
 
 def __prepare_prediction(run, example):
     return {"prediction": run.outputs.get("output")}
@@ -42,9 +38,9 @@ def __execute_prompt(input: Dict[str, str], input_key: str) -> Dict[str, str]:
     text_message = prompt.format(**{input_key: input[input_key]})
     
     response = open_ai.chat.completions.create(
-        model = MODEL_NAME,
+        model = getenv("MODEL_NAME"),
         messages = [{"role": "user", "content": text_message}],
-        temperature = TEMPERATURE
+        temperature = float(getenv("TEMPERATURE", 0.0))
     )
 
     return {"output": response.choices[0].message.content}
@@ -63,13 +59,13 @@ def __validate_schema(run, esx):
     except ValidationError as e:
         return {"score": 0.0, "details": f"JSON decode error: {str(e)}"}
 
-dataset_preparation_report  =  prepare_dataset(dataset_name = DATASET_NAME, 
+dataset_preparation_report  =  prepare_dataset(dataset_name = getenv("DATASET_NAME"),
                                                client = LangSmithClient(), 
                                                dataset_file_path = Path(__file__).parent / getenv("DATASET_FILE_NAME", "json_schema_validation_dataset.jsonl"))
 logger.info(f"Dataset preparation report: {dataset_preparation_report}")
 
 result = evaluate(__format_evaluation_input, 
-                  data = DATASET_NAME, 
+                  data = getenv("DATASET_NAME"), 
                   evaluators = [__validate_schema, LangChainStringEvaluator("json_validity", prepare_data = __prepare_prediction)], 
                   experiment_prefix = "FormatEvaluation", 
                   max_concurrency = 1)
